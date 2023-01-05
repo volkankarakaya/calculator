a9ee858230aa76b8b89de352b19320ef3eba8b6c
Alberto-Knox <volkankarakaya2020@gmail.com>
2023-01-05 13:44:27 +0300

    add first and second number sections to the display

--- app.js
+++ app.js
@@ -1,4 +1,72 @@
-let numbers = document.querySelectorAll('.number');
-let operators = document.querySelectorAll('.operator');
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equalBtn= document.getElementById('operator-equal-btn');
+let display = document.getElementById('display');
 
+let firstNumber = "";
+let secondNumber = "";
+let operator = "";
+
+operators.forEach((op) => {
+  op.addEventListener("click", (e) => {
+    operator = e.target.innerText;
+    console.log(operator);
+  });
+});
+
+
+numbers.forEach((number) => {
+  number.addEventListener("click", (e) => {
+    if (operator === "") {
+      if (firstNumber === "") {
+        firstNumber = e.target.innerText;
+      } else {
+        firstNumber = `${firstNumber}${e.target.innerText}`;
+      }
+      console.log('firstnumber', firstNumber)
+      console.log('operator', operator)
+    } else {
+      if(secondNumber===""){
+        secondNumber = e.target.innerText;
+      }else{
+        secondNumber = `${secondNumber}${e.target.innerText}`
+      }
+      console.log('secondnumber', secondNumber)
+      console.log('operator', operator)
+    }
+  });
+});
+
+function operate(firstNumber,secondNumber,operator){
+  let result
+  firstNumber = parseInt(firstNumber);
+  secondNumber = parseInt(secondNumber);
+  switch (operator){
+    case '+':
+      result = firstNumber + secondNumber;
+      console.log('result', result);
+
+      firstNumber = parseInt(result);
+      secondNumber= ''
+      
+      break;
+    case '-':
+      result = firstNumber - secondNumber;
+      console.log(result);
+      break;
+    case '/':
+      result = firstNumber / secondNumber;
+      console.log(result);
+      break;
+    case '*':
+      result = firstNumber * secondNumber;
+      console.log(result);
+      break;
+  }
+  
+  
+
+}
+
+equalBtn.addEventListener('click', ()=> operate( firstNumber,secondNumber,operator))
 
